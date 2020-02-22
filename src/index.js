193cb72b59c23e685ce68dfcf807802360c858ec
Matheus Araujo <matheusnewff@gmail.com>
2020-02-22 17:55:21 -0300

    app conectado ao banco Mongo Db na nuvem

--- src/index.js
+++ src/index.js
@@ -1,5 +1,14 @@
 //importando o express
-const express = require('express')
+const express = require('express');
+
+//importando a lib mongose
+const mongose = require('mongoose');
+
+//criando a conexão com o banco
+mongose.connect('mongodb+srv://admin:admin@cluster0-zpexf.mongodb.net/test?retryWrites=true&w=majority', {
+    useNewUrlParser: true,
+});
+
 
 //criandp a aplicação
 const app = express();
